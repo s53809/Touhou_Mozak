3aaba6fd35f1ff3cda8a9624da6325d212e97925
Minsoda <dg4res@gmail.com>
2024-11-09 17:25:28 +0900

    11/09 : 노트북 데스크탑 동기화용 커밋

---
+++ Touhou/Assets/Scripts/Character/Player.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Single defaultPlayerSpeed = 1.0f;
+
+    public MonoObjectPooler bulletShooter;
+
+    [SerializeField] private Single playerSpeed = 0;
+    public void Update()
+    {
+        
+    }
+
+    public void PlayerMove()
+    {
+
+    }
+}
