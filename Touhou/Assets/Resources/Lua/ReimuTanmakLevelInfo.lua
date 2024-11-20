a99239cc1f02aaaacb91d69476f6b1cbe91d1817
Minsoda <dg4res@gmail.com>
2024-11-20 22:59:47 +0900

    11/20 : 데스크탑 노트북 동기화용 커밋

--- Touhou/Assets/Resources/Lua/ReimuTanmakLevelInfo.lua
+++ Touhou/Assets/Resources/Lua/ReimuTanmakLevelInfo.lua
@@ -28,13 +28,13 @@ reimuTanmakLevels = {
     {
         levels.new("ReimuDefaultTanmak", 3, 0.1,
         VArray({Vector2(-0.4, 0.8), Vector2(0, 0.8), Vector2(0.4, 0.8)}),
-        VArray({Vector2.up, Vector2.up, Vector2.up})
+        VArray({Vector2.up, Vector2.down, Vector2.up})
         ),
-        levels.new("ReimuFollowTanmak", 2, 0.05,
+        levels.new("ReimuFollowTanmak", 2, 0.4,
         VArray({Vector2(-0.5, 0), Vector2(0.5, 0)}),
         VArray({
-            Vector2Util.RotateVector(Vector2.down, 25),
-            Vector2Util.RotateVector(Vector2.down, -25)
+            Vector2Util.RotateVector(Vector2.up, 45),
+            Vector2Util.RotateVector(Vector2.up, -45)
         })
         )
     }
